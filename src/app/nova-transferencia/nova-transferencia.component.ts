f44c17e9fa1c1096e03777d9c64faba68b286992
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-29 10:12:24 -0300

    Criando objeto para exportação de valores

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,12 +7,14 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor!: number
   destino!: number;
 
   transferir() {
     console.log('solicitado nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    this.aoTransferir.emit({valor: this.valor, destino: this.destino})
+
   }
 }
